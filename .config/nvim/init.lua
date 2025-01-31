07a9df69189cf236993ba32ac421d82e2994ae2e
ghxst0x1 <61968324+ghxst0x1@users.noreply.github.com>
2025-01-31 19:18:42 +0530

    config changes Updated hyprland config Updated ohmyposh config Updated neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,9 @@ vim.o.mouse = "a"
 vim.o.clipboard = "unnamedplus"
 
 -- Set terminal gui colors to true
---vim.o.termguicolors = true
+vim.o.termguicolors = true
+
+vim.o.undofile = true
 
 -- Tabs vs spaces
 vim.o.tabstop = 2      -- A TAB character looks like 4 spaces
@@ -27,12 +29,16 @@ vim.o.softtabstop = 2  -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2   -- Number of spaces inserted when indenting
 vim.o.cursorline = true
 
--- Keymaps
-vim.api.nvim_set_keymap("n", "<leader>tt", ":NvimTreeToggle<enter>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFocus<enter>", {noremap=true})
 
 require("plugins.lazy")
+require("mappings")
 
 -- Set colorscheme
 --vim.cmd [[colorscheme onedark]]
 vim.cmd.colorscheme "catppuccin"
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.o.lazyredraw = true
+vim.o.updatetime = 300
+vim.o.timeoutlen = 500
---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,7 @@
+-- Keymaps
+vim.api.nvim_set_keymap("n", "<leader>tt", ":NvimTreeToggle<enter>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFocus<enter>", {noremap=true})
+
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<enter>", {noremap=true})
+
+
